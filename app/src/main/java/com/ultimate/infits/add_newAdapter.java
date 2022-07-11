3a81b2f1a9945e26678fd7e9c9bbf1f0504cb54b
muskan2403 <muskan2000.24@gmail.com>
2022-07-11 19:29:28 +0530

    recepies

---
+++ app/src/main/java/com/ultimate/infits/add_newAdapter.java
@@ -0,0 +1,56 @@
+package com.ultimate.infits;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class add_newAdapter extends RecyclerView.Adapter<add_newAdapter.add_newHolder>{
+
+    Context ct;
+    List<add_new> obj;
+
+    public add_newAdapter(Context ct, List<add_new> obj) {
+        this.ct = ct;
+        this.obj = obj;
+    }
+
+    @NonNull
+    @Override
+    public add_newHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(ct);
+        View view = layoutInflater.inflate(R.layout.add_new2,parent,false);
+        return new add_newHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull add_newHolder holder, int position) {
+        add_new pos = obj.get(position);
+
+        holder.name.setText(pos.getName());
+        holder.quantity.setText(pos.getQuantity());
+        holder.amount.setText(pos.getQuantity());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return obj.size();
+    }
+
+    public class add_newHolder extends RecyclerView.ViewHolder {
+        EditText name,quantity,amount;
+        public add_newHolder(View view) {
+            super(view);
+            name = view.findViewById(R.id.food_name);
+            quantity = view.findViewById(R.id.food_quantity);
+            amount = view.findViewById(R.id.food_amount);
+        }
+    }
+}
--- app/src/main/java/com/ultimate/infits/food_details.java
+++ app/src/main/java/com/ultimate/infits/food_details.java
@@ -1,16 +1,23 @@
 package com.ultimate.infits;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class food_details extends AppCompatActivity {
 
     Spinner category,cookingTime;
+    RecyclerView ingredients_food,nutritions,utensis_food;
+    List<add_new> ingred= new ArrayList<>();
+    List<add_new> nutrit= new ArrayList<>();
+    List<add_new> utensils= new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +25,9 @@ public class food_details extends AppCompatActivity {
 
         category=findViewById(R.id.category);
         cookingTime = findViewById(R.id.cookingTime);
+        ingredients_food = findViewById(R.id.ingredients_food);
+        nutritions = findViewById(R.id.nutritions);
+        utensis_food = findViewById(R.id.utensils_food);
 
         ArrayList<String> time = new ArrayList<>();
         time.add("Breakfast");
@@ -37,5 +47,23 @@ public class food_details extends AppCompatActivity {
         ArrayAdapter<String> spinnerArrayAdapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,duration );
         spinnerArrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
         cookingTime.setAdapter(spinnerArrayAdapter2);
+
+        add_new addNew = new add_new("cholestrol","mg","10");
+        nutrit.add(addNew);
+        add_newAdapter obj = new add_newAdapter(getApplicationContext(),nutrit);
+        nutritions.setAdapter(obj);
+        nutritions.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        add_new addNew2 = new add_new("olive oil","tbsp","5");
+        ingred.add(addNew2);
+        add_newAdapter obj2 = new add_newAdapter(getApplicationContext(),ingred);
+        ingredients_food.setAdapter(obj2);
+        ingredients_food.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        add_new addNew3 = new add_new("cutting Board","","1");
+        utensils.add(addNew3);
+        add_newAdapter obj3 = new add_newAdapter(getApplicationContext(),utensils);
+        utensis_food.setAdapter(obj3);
+        utensis_food.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
     }
 }
